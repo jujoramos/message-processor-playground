0edeb160f69e1cd16be007488ce88f55b3134da6
Juan José Ramos Cassella <jujoramos@gmail.com>
2024-05-21 20:52:17 +0100

    feat: Set Up Picocli
    Use "picocli" as the command line interface framework.

--- app/src/main/kotlin/com/logitech/app/App.kt
+++ app/src/main/kotlin/com/logitech/app/App.kt
@@ -1,5 +1,33 @@
 package com.logitech.app
 
-fun main() {
-	println("Hello World!")
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import picocli.CommandLine
+import picocli.CommandLine.Command
+import picocli.CommandLine.Option
+import java.util.concurrent.Callable
+import kotlin.system.exitProcess
+
+fun main(args: Array<String>): Unit = exitProcess(CommandLine(HelloWorld()).execute(*args))
+
+@Command(
+    name = "Hello World",
+    mixinStandardHelpOptions = true,
+    version = ["1.0"],
+    description = ["Say hello"]
+)
+class HelloWorld : Callable<Int> {
+    private companion object {
+        val logger: Logger = LoggerFactory.getLogger(this::class.java.enclosingClass)
+    }
+
+    @Option(names = ["-n", "--name"], description = ["Say your name"])
+    var name = "World"
+
+    override fun call(): Int {
+        logger.info("Say hello to $name")
+        println("Hello $name")
+
+        return 0
+    }
 }
--- buildSrc/src/main/kotlin/buildlogic.kotlin-application-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/buildlogic.kotlin-application-conventions.gradle.kts
@@ -1,4 +1,12 @@
+import gradle.kotlin.dsl.accessors._c5a1b2808732718201c96d23252852b9.implementation
+import org.gradle.accessors.dm.LibrariesForLibs
+
 plugins {
     id("buildlogic.kotlin-common-conventions")
     application
+}
+
+val libs = the<LibrariesForLibs>()
+dependencies {
+    implementation(libs.picocli)
 }
